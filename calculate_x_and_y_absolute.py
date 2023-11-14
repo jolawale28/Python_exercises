f8e17e8bcff373d7ba86ceeea54f2b9bdf98112f
jolawale28 <jolawale28@gmail.com>
2023-11-14 16:25:57 +0100

    Create calculate_x_and_y_absolute.py

---
+++ calculate_x_and_y_absolute.py
@@ -0,0 +1,9 @@
+# Write a program that asks the user to enter two numbers, x and y, and computes |x - y|/x + y
+
+# 2023 Joseph Olawale Bamidele
+
+input_x = int(input("Please enter x: "))
+input_y = int(input("Please enter y: "))
+
+print(abs(input_x - input_y)/(input_x + input_y))
+
